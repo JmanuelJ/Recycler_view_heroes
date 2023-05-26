e657b90699f51c6d80485ab4febbcf05cc84fb99
JmanuelJ <47757280+JmanuelJ@users.noreply.github.com>
2023-05-25 21:13:27 -0500

    Cards

---
+++ RecyclerViewExample/app/src/main/java/com/myapplication/adapter/SuperHeroAdapter.kt
@@ -0,0 +1,22 @@
+package com.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.myapplication.R
+import com.myapplication.SuperHero
+
+class SuperHeroAdapter(private val superheroList: List<SuperHero>, private val onClickListener:(SuperHero) -> Unit): RecyclerView.Adapter<SuperHeroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SuperHeroViewHolder(layoutInflater.inflate(R.layout.item_superhero,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+        val item = superheroList[position]
+        holder.render(item,onClickListener)
+    }
+
+    override fun getItemCount(): Int =superheroList.size
+}
---
+++ RecyclerViewExample/app/src/main/java/com/myapplication/adapter/SuperHeroViewHolder.kt
@@ -0,0 +1,20 @@
+package com.myapplication.adapter
+
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.myapplication.SuperHero
+import com.myapplication.databinding.ItemSuperheroBinding
+
+class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    val binding = ItemSuperheroBinding.bind(view)
+
+    fun render(superHeroModel: SuperHero, onClickListener:(SuperHero) -> Unit){
+        binding.tvSuperHeroName.text = superHeroModel.superhero
+        binding.tvRealName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+        itemView.setOnClickListener{ onClickListener(superHeroModel) }
+    }
+}
